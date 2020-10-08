d13e2f08ed2a220f93baac0d94f799e368bb7211
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-10-08 18:27:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,3 @@
-def fizzbuzz(3)
-  if 3 % 3 == 0
-    return fizz_3 = fizzbuzz(3)
-  end
-end
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
