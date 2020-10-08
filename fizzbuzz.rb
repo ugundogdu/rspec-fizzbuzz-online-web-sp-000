b363fdc845c033a9bb83161085223b46f710392d
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-10-08 18:51:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,5 @@
-fizzbuzz(3) == "Fizz"
+def fizzbuzz
 
-def fizzbuzz(3)
-  if 3 % 3 == 0
-    puts fizz_3 = fizzbuzz(3)
-  end
-
-  fizzbuzz(3)
+end
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
