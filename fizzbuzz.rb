cc0b63ce4510fbd0428cdca5539c3078efe32a3f
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-10-08 19:01:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,5 +3,11 @@ def fizzbuzz(int)
     "Fizz"
   end
 end
+
+def fizzbuzz(int)
+  if int % 5 == 0
+    "buzz"
+  end
+end
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
