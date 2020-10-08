979643bc7fbaf454260a1bd4581a0b4e1985e1ae
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-10-08 18:47:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,10 @@
-fizzbuzz(int) == "Fizz"
+fizzbuzz(3) == "Fizz"
 
-def fizzbuzz(int)
-  if int % 3 == 0
+def fizzbuzz(3)
+  if 3 % 3 == 0
     puts "Fizz"
   end
+
+  fizzbuzz(3)
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
