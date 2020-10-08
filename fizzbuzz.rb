6c765fa4f7e58973926ee7aefc69ff4a7c27aa59
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-10-08 18:41:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,7 @@
-require_relative
-def fizzbuzz(int)
-  if int % 3 == 0
+def fizzbuzz(3)
+  if 3 % 3 == 0
     puts fizz_3 = fizzbuzz(3)
   end
-
-  def fizzbuzz(3)
-    (fizz_3) .to eq("Fizz")
-  end
+  
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
